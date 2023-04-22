f33591235512c162ed8296b623a18fad55965c65
Stuart Rimel <stuart.rimel@gmail.com>
2023-04-22 02:02:37 -0700

    Fixed clippy, add comments

--- src/lib.rs
+++ src/lib.rs
@@ -1,28 +1,44 @@
+//! # Toy RSA Library
+//! ### Stuart Rimel, Rust, Spring 2023
+//! 
+//! This library provides RSA key generation, encryption and decryption. 
+//! 
+//! This crate is an exercise and should not be used for anything that
+//! needs to be kept secure in real-life.
+
 use toy_rsa_lib::*;
 
+/// Fixed RSA encryption exponent.
 pub const EXP: u64 = 65_537;
 
+/// Returns the least common multple of `p-1` and `q-1`.
 fn carmichael_totient(p: u64, q: u64) -> u64 {
-    toy_rsa_lib::lcm(p - 1, q - 1)
+    lcm(p - 1, q - 1)
 }
 
+/// Generates a pair of primes in the range `2**31..2**32`
+/// suitable for RSA encryption with exponent. 
 pub fn genkey() -> (u32, u32) {
-    let (mut p, mut q) = (0, 0);
+    let (mut p, mut q);
     loop {
         p = rsa_prime();
         q = rsa_prime();
-        let lambda = carmichael_totient(p as u64, q as u64);
-        if EXP < lambda && gcd(EXP, lambda) == 1 {
+        let totient = carmichael_totient(p as u64, q as u64);
+        if EXP < totient && gcd(EXP, totient) == 1 {
             break;
         }
     }
     (p, q)
 }
 
+/// Encrypt the plaintext `msg` using the RSA public `key`
+/// and return the ciphertext.
 pub fn encrypt(key: u64, msg: u32) -> u64 {
     modexp(msg as u64, EXP, key)
 }
 
+/// Decrypt the ciphertext `msg` using the RSA private `key`
+/// and return the resulting plaintext.
 pub fn decrypt(key: (u32, u32), msg: u64) -> u32 {
     let d = modinverse(EXP, carmichael_totient(key.0 as u64, key.1 as u64));
     modexp(msg, d, key.0 as u64 * key.1 as u64) as u32
@@ -46,6 +62,7 @@ mod tests {
         assert_ne!(p, q);
     }
 
+    /// Checks primality of `n` and returns a boolean result.
     fn is_prime(n: u32) -> bool {
         if n < 2 {
             return false;
@@ -70,7 +87,7 @@ mod tests {
     }
 
     #[test]
-    fn testing_decrypt() {
+    fn testing_decrypt_from_example() {
         let msg = 0x12345fu32;
         let private_key = (0xed23e6cdu32, 0xf050a04du32);
         let public_key = private_key.0 as u64 * private_key.1 as u64;
